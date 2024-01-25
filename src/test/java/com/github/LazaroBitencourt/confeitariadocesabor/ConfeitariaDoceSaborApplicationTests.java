1c850f9cc0b470be323381fa2ea2e4da81cf60a5
[name] <[email address]>
2024-01-25 20:44:34 -0300

    primeiro commit

---
+++ src/test/java/com/github/LazaroBitencourt/confeitariadocesabor/ConfeitariaDoceSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LazaroBitencourt.confeitariadocesabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaDoceSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
