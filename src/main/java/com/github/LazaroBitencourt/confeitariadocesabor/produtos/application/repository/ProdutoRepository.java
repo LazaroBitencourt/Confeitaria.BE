4420268e5221037f568ca0f0597efd138a201ede
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-26 22:00:06 -0300

    cria contrato de service em repository

---
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/application/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.github.LazaroBitencourt.confeitariadocesabor.produtos.application.repository;
+
+import com.github.LazaroBitencourt.confeitariadocesabor.produtos.domain.Produto;
+
+public interface
+ProdutoRepository {
+    Produto save(Produto produto);
+}
