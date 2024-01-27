cba04574dccc5a104bd78b2b2c9ec45d2cbc363d
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-26 22:20:29 -0300

    testa fluxo, ok

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/infra/ProdutoInfraRepository.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/infra/ProdutoInfraRepository.java
@@ -9,14 +9,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 @Log4j2
-public class ProdutoinfraRepository implements ProdutoRepository {
+public class ProdutoInfraRepository implements ProdutoRepository {
 
     private final ProdutoSpringDataJpaRepository jpaRepository;
     @Override
     public Produto save(Produto novoProduto) {
-        log.info("[inicia] ProdutoinfraRepository - save");
+        log.info("[inicia] ProdutoInfraRepository - save");
         var produto = jpaRepository.save(novoProduto);
-        log.info("[finaliza] ProdutoinfraRepository - save");
+        log.info("[finaliza] ProdutoInfraRepository - save");
         return produto;
     }
 }
