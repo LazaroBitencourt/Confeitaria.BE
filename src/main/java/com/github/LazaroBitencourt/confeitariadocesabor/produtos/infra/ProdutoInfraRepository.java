930b7508039a5b129df254f3b9aac4f171c859c8
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-01-27 17:12:53 -0300

    revisa implementacao, implementa try-cacth no metodo save

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/infra/ProdutoInfraRepository.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/infra/ProdutoInfraRepository.java
@@ -1,9 +1,12 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.produtos.infra;
 
+import com.github.LazaroBitencourt.confeitariadocesabor.handler.APIException;
 import com.github.LazaroBitencourt.confeitariadocesabor.produtos.application.repository.ProdutoRepository;
 import com.github.LazaroBitencourt.confeitariadocesabor.produtos.domain.Produto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,8 +18,12 @@ public class ProdutoInfraRepository implements ProdutoRepository {
     @Override
     public Produto save(Produto novoProduto) {
         log.info("[inicia] ProdutoInfraRepository - save");
-        var produto = jpaRepository.save(novoProduto);
+        try{
+            jpaRepository.save(novoProduto);
+        }catch (DataIntegrityViolationException e){
+            throw APIException.build(HttpStatus.BAD_REQUEST,"EXISTE DADOS DUPLICADOS", e);
+        }
         log.info("[finaliza] ProdutoInfraRepository - save");
-        return produto;
+        return novoProduto;
     }
 }
