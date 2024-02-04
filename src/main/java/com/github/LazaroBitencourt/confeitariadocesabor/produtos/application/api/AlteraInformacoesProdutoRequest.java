15673fbc6b17c68eaa144b742e75d4aa36427c6d
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-04 17:21:44 -0300

    cria e implementa classe altera-informacoes-produto-request

---
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/produtos/application/api/AlteraInformacoesProdutoRequest.java
@@ -0,0 +1,16 @@
+package com.github.LazaroBitencourt.confeitariadocesabor.produtos.application.api;
+
+import com.github.LazaroBitencourt.confeitariadocesabor.produtos.domain.Categoria;
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+public class AlteraInformacoesProdutoRequest {
+    private String nome;
+    private String descricao;
+    private Categoria categoria;
+    private LocalDate dataFabricacao;
+    private LocalDate dataValidade;
+    private Double preco;
+}
