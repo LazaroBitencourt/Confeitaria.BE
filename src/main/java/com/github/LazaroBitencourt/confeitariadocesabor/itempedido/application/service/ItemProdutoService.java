1e619fc79967786589cb380635aac32ef6c53756
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-08 11:22:47 -0300

    cria contrato de controller em service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/itempedido/application/service/ItemProdutoService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/itempedido/application/service/ItemProdutoService.java
@@ -1,4 +1,7 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.service;
 
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.api.ItemPedidoRequest;
+
 public interface ItemProdutoService {
+    void cadastraItemPedido(ItemPedidoRequest itemPedidoRequest);
 }
