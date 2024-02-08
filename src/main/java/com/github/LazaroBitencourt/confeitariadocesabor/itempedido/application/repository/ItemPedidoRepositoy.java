57a88f246fc31da4b602d2b58891a4d6fe3aa295
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-08 12:26:29 -0300

    cria contrato de service em repository

---
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/itempedido/application/repository/ItemPedidoRepositoy.java
@@ -0,0 +1,8 @@
+package com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.repository;
+
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.api.ItemPedidoRequest;
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.domain.ItemPedido;
+
+public interface ItemPedidoRepositoy {
+    ItemPedido salva(ItemPedidoRequest novoItemPedido);
+}
