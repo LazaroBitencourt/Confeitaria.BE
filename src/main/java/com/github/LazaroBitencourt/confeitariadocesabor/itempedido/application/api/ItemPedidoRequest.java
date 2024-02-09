957ae30ada1af76cb5bb9ab604fe937d19795639
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-09 18:36:11 -0300

    modifica item-pedido-request

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/itempedido/application/api/ItemPedidoRequest.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/itempedido/application/api/ItemPedidoRequest.java
@@ -1,18 +1,15 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.api;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.produtos.domain.Produto;
 import lombok.Getter;
-
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class ItemPedidoRequest {
 
-    @NotNull
-    private Produto idProduto;
+    @NotNull(message = "ID do Produto deve ser definido")
+    private Long idProduto;
     @NotNull(message = "Quantidades de produto deve ser definida")
     private int quantidade;
-
 
 }
