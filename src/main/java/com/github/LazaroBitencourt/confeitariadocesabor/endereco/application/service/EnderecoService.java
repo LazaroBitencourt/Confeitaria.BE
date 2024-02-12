52c8211a154909b3903c2b12ee9729444ff6584f
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-12 18:20:54 -0300

    cria contrato de controller em service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/service/EnderecoService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/service/EnderecoService.java
@@ -1,9 +1,14 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.service;
 
+import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api.DetalhaEnderecoResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api.EnderecoIdResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api.EnderecoRequest;
+
+import java.util.UUID;
 
 public interface EnderecoService {
 
     EnderecoIdResponse cadastraEndereco(EnderecoRequest enderecoRequest);
+
+    DetalhaEnderecoResponse buscaEnderecoPorId(UUID idEndereco);
 }
