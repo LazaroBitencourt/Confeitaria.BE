f7b4a0bd087e7b7e9107ac6571afde47c53f16ac
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-12 18:02:37 -0300

    implementa camada api

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/api/EnderecoAPI.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/api/EnderecoAPI.java
@@ -1,12 +1,10 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RequestMapping("/endereco")
 public interface EnderecoAPI {
@@ -14,4 +12,9 @@ public interface EnderecoAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public EnderecoIdResponse postCadastraEndereco(@RequestBody @Valid EnderecoRequest enderecoRequest);
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public DetalhaEnderecoResponse getBuscaEnderecoPorId(@PathVariable("id") UUID idEndereco);
+
 }
