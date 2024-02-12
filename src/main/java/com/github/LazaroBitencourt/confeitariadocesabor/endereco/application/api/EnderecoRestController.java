8140ab00b66b3795ee572fadd0208a227687f0c2
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-12 17:01:27 -0300

    implementa camada controller

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/api/EnderecoRestController.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/endereco/application/api/EnderecoRestController.java
@@ -1,5 +1,6 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api;
 
+import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.service.EnderecoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class EnderecoRestController implements EnderecoAPI{
 
+    private final EnderecoService service;
+
     @Override
     public EnderecoIdResponse postCadastraEndereco(EnderecoRequest enderecoRequest) {
         log.info("[inicia] EnderecoRestController - postCadastraEndereco");
+        EnderecoIdResponse enderecoIdResponse = service.cadastraEndereco(enderecoRequest);
         log.info("[finaliza] EnderecoRestController - postCadastraEndereco");
-        return null;
+        return enderecoIdResponse;
     }
 }
