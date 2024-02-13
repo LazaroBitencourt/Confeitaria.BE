eeb144275f46fd9dfdd57fb1e0ddb5723fda4012
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-13 13:24:29 -0300

    implementa cliente-application-service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteApplicationService.java
@@ -2,11 +2,14 @@ package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.ser
 
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClientRequest;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteIdResponse;
+import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.DetalhaClienteResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.repository.ClienteRepository;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +24,13 @@ public class ClienteApplicationService implements ClienteService{
         Cliente novoCliente = repository.salva(new Cliente(clientRequest));
         log.info("[finaliza] ClienteApplicationService - cadastraNovoCliente");
         return ClienteIdResponse.builder().idCliente(novoCliente.getIdCliente()).build();
+    }
+
+    @Override
+    public DetalhaClienteResponse detalhaClientePorId(UUID idCliente) {
+        log.info("[inicia] ClienteApplicationService - detalhaClientePorId");
+        Cliente cliente = repository.detalhaClientePorId(idCliente);
+        log.info("[finaliza] ClienteApplicationService - detalhaClientePorId");
+        return new DetalhaClienteResponse(cliente);
     }
 }
