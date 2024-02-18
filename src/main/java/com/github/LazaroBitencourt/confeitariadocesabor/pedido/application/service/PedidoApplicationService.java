1f468cb27d0ded105e2950e8f9c5e3e7aa1b155a
[name] <[email address]>
2024-02-18 14:42:56 -0300

    implementa pedido-application-service testa fluxo, ok

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/service/PedidoApplicationService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/service/PedidoApplicationService.java
@@ -2,6 +2,10 @@ package com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.serv
 
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.repository.ClienteRepository;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Cliente;
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.api.ItemPedidoIdResponse;
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.repository.ItemPedidoRepositoy;
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.application.service.ItemPedidoApplicationService;
+import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.domain.ItemPedido;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api.PedidoIdResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api.PedidoRequest;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.repository.PedidoRepository;
@@ -10,6 +14,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -17,13 +25,33 @@ public class PedidoApplicationService implements PedidoService{
 
     private final PedidoRepository repository;
     private final ClienteRepository clienteRepository;
+    private final ItemPedidoApplicationService itemPedidoService;
+    private final ItemPedidoRepositoy itemPedidoRepositoy;
 
     @Override
     public PedidoIdResponse cadastraPedido(PedidoRequest novoPedido) {
         log.info("[inicia] PedidoApplicationService - cadastraPedido");
         Cliente cliente = clienteRepository.buscaClientePorId(novoPedido.getCliente());
-        Pedido pedido = repository.salva(new Pedido( cliente, novoPedido));
+        List<ItemPedidoIdResponse> listaDeitensPedidocadastrados = cadastraItensPedido(novoPedido);
+        List<ItemPedido> listaDeItemPedido = buscaItensPedidocadastrados(listaDeitensPedidocadastrados);
+        Pedido pedido = repository.salva(new Pedido( cliente,listaDeItemPedido, novoPedido));
         log.info("[finaliza] PedidoApplicationService - cadastraPedido");
         return PedidoIdResponse.builder().idPedido(pedido.getIdPedido()).build();
+    }
+
+    private List<ItemPedidoIdResponse> cadastraItensPedido(PedidoRequest itensDePedido) {
+        return itensDePedido.getItensDePedido()
+                .stream()
+                .map(itemPedido
+                        -> itemPedidoService.cadastraItemPedido(itemPedido))
+                .collect(Collectors.toList());
+    }
+
+    private List<ItemPedido> buscaItensPedidocadastrados(List<ItemPedidoIdResponse> itensPedido) {
+        return itensPedido
+                .stream()
+                .map(idItemPedido
+                        -> itemPedidoRepositoy.buscaItemPedidoPorId(idItemPedido.getIdItemPedido()))
+                .collect(Collectors.toList());
     }
 }
