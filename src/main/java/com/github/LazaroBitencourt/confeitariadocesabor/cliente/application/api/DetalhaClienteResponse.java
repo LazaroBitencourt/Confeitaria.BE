706f2d39a009d3e8baf658102f86c29ea270b7d8
[name] <[email address]>
2024-02-17 23:00:42 -0300

    modifica detalha-cliente-response, adiciona atributo idCliente

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/DetalhaClienteResponse.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/DetalhaClienteResponse.java
@@ -3,14 +3,18 @@ package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Cliente;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class DetalhaClienteResponse {
 
+    private UUID idCliente;
     private String nome;
     private String telefone;
     private ClienteEndereco endereco;
 
     public DetalhaClienteResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
         this.nome = cliente.getNome();
         this.telefone = cliente.getTelefone();
         this.endereco = new ClienteEndereco(cliente.getEndereco());
