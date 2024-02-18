6dd1473bdd4ff2a6bc14abbd185e7eeb97eefed7
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-17 21:23:19 -0300

    modifica detalha-cliente-rest-controller

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/DetalhaClienteResponse.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/DetalhaClienteResponse.java
@@ -1,7 +1,6 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api;
 
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Cliente;
-import com.github.LazaroBitencourt.confeitariadocesabor.endereco.domain.Endereco;
 import lombok.Getter;
 
 @Getter
@@ -15,23 +14,6 @@ public class DetalhaClienteResponse {
         this.nome = cliente.getNome();
         this.telefone = cliente.getTelefone();
         this.endereco = new ClienteEndereco(cliente.getEndereco());
-    }
-
-    private class ClienteEndereco {
-
-        private String logadouro;
-        private String numero;
-        private String bairro;
-        private String complemento;
-        private String cep;
-
-        public ClienteEndereco(Endereco endereco) {
-            this.logadouro = endereco.getLogadouro();
-            this.numero = endereco.getNumero();
-            this.bairro = endereco.getBairro();
-            this.complemento = endereco.getComplemento();
-            this.cep = endereco.getCep();
-        }
     }
 
 }
