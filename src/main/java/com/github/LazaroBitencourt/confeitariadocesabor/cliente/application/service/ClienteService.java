7669580809fab2253e7d5ee03e30e73c3d336dd6
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-17 21:25:48 -0300

    modifica cliente-service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteService.java
@@ -1,13 +1,13 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.service;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClientRequest;
+import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteRequest;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteIdResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.DetalhaClienteResponse;
 
 import java.util.UUID;
 
 public interface ClienteService {
-    ClienteIdResponse cadastraNovoCliente(ClientRequest clientRequest);
+    ClienteIdResponse cadastraNovoCliente(ClienteRequest clientRequest);
 
     DetalhaClienteResponse detalhaClientePorId(UUID idCliente);
 }
