6064999ed91be305f2b33edbddd1483aad6e6869
[name] <[email address]>
2024-02-18 14:38:51 -0300

    cria e implementa pedido-id-response

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/PedidoIdResponse.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/PedidoIdResponse.java
@@ -1,12 +1,14 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Value;
 
 import java.util.UUID;
 
 @Builder
 @Value
+@Getter
 public class PedidoIdResponse {
     private UUID idPedido;
 }
