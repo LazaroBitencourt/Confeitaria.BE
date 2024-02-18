8f533ee7f1c423f0c78fe7fa06478323a7504fa6
[name] <[email address]>
2024-02-18 14:59:32 -0300

    move item-pedido-jpa e item-pedido-spring-data-jpa-repository para camada de infra em pedido

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/infra/ItemPedidoSpringDataJpaRepository.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/infra/ItemPedidoSpringDataJpaRepository.java
@@ -1,6 +1,6 @@
-package com.github.LazaroBitencourt.confeitariadocesabor.itempedido.infra;
+package com.github.LazaroBitencourt.confeitariadocesabor.pedido.infra;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.itempedido.domain.ItemPedido;
+import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemPedidoSpringDataJpaRepository extends JpaRepository<ItemPedido,Long> {
