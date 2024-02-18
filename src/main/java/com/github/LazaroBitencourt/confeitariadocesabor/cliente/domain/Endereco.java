5b4600529f011f8ddbe01ab3dd116a42292f5246
[name] <[email address]>
2024-02-17 22:22:56 -0300

    modifica entidade endereco

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/domain/Endereco.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/domain/Endereco.java
@@ -1,8 +1,6 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain;
 
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteRequest;
-import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api.AlteraEnderecoRequest;
-import com.github.LazaroBitencourt.confeitariadocesabor.endereco.application.api.EnderecoRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,28 +28,12 @@ public class Endereco {
     @NotBlank
     private String cep;
 
-    public Endereco(EnderecoRequest enderecoRequest) {
-        this.logadouro = enderecoRequest.getLogadouro();
-        this.numero = enderecoRequest.getNumero();
-        this.bairro = enderecoRequest.getBairro();
-        this.complemento = enderecoRequest.getComplemento();
-        this.cep = enderecoRequest.getCep();
-    }
-
     public Endereco(ClienteRequest clienteRequest) {
         this.logadouro = clienteRequest.getEndereco().getLogadouro();
         this.numero = clienteRequest.getEndereco().getNumero();
         this.bairro = clienteRequest.getEndereco().getBairro();
         this.complemento = clienteRequest.getEndereco().getComplemento();
         this.cep = clienteRequest.getEndereco().getCep();
-    }
-
-    public void alteraInformacoesDoEndereco(AlteraEnderecoRequest informacoesDoEndereco) {
-        this.logadouro = informacoesDoEndereco.getLogadouro();
-        this.numero = informacoesDoEndereco.getNumero();
-        this.bairro = informacoesDoEndereco.getBairro();
-        this.complemento = informacoesDoEndereco.getComplemento();
-        this.cep = informacoesDoEndereco.getCep();
     }
 
 }
