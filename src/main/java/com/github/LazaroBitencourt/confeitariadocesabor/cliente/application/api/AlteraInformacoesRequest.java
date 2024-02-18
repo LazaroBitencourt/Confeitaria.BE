267dfd6e70010316be4677b30910b00665db8496
[name] <[email address]>
2024-02-18 00:20:21 -0300

    modifica altera-informacoes-request da camada api

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/AlteraInformacoesRequest.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/AlteraInformacoesRequest.java
@@ -1,6 +1,5 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Endereco;
 import lombok.Value;
 
 @Value
@@ -8,5 +7,5 @@ public class AlteraInformacoesRequest {
 
     private String nome;
     private String telefone;
-    private Endereco endereco;
+    private ClienteEndereco endereco;
 }
