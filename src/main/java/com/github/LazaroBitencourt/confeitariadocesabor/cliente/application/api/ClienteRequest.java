0c5cf6d64348e13f42143645c349ebc3f8e18d87
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-17 21:20:47 -0300

    modifica cliente-request

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/ClienteRequest.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/api/ClienteRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Value
-public class ClientRequest {
+public class ClienteRequest {
 
     @NotNull
     private String nome;
