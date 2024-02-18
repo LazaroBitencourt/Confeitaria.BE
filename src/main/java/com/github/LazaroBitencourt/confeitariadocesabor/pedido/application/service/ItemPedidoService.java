d6d115b71d3446693af502013a5e95b74e81b7f8
[name] <[email address]>
2024-02-18 16:33:21 -0300

    modifica retorno em item-pedido-service da camada service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/service/ItemPedidoService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/service/ItemPedidoService.java
@@ -1,11 +1,12 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.service;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api.ItemPedidoIdResponse;
+
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api.ItemPedidoRequest;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api.ItemPedidoResponse;
+import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.ItemPedido;
 
 public interface ItemPedidoService {
-    ItemPedidoIdResponse cadastraItemPedido(ItemPedidoRequest itemPedidoRequest);
+    ItemPedido cadastraItemPedido(ItemPedidoRequest itemPedidoRequest);
 
     ItemPedidoResponse buscaItemPedidoPorId(Long idItemProduto);
 
