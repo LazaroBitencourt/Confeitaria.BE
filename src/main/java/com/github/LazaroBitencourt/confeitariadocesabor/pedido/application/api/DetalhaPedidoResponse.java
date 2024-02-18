0f0498a67c29215c0655fd98687ed406f7d6c439
[name] <[email address]>
2024-02-18 18:31:02 -0300

    implementa construtor em detalha-pedido-response da camada api

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/DetalhaPedidoResponse.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/DetalhaPedidoResponse.java
@@ -1,6 +1,7 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api;
 
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.ItemPedido;
+import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.Pedido;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.TipoDeEntrega;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.TipoDePagamento;
 import lombok.Getter;
@@ -20,4 +21,24 @@ public class DetalhaPedidoResponse {
     private TipoDePagamento formaDePagamento;
     private double valorTotal;
     private EnderecoDeEntrega  endereco;
+
+    public DetalhaPedidoResponse(Pedido pedido) {
+        this.idPedido = pedido.getIdPedido();
+        this.cliente = converteTipoClienteParaInformacoesCliente(pedido);
+        this.dataHoraDoPedido = pedido.getDataHoraDoPedido();
+        this.dataHoraParaEntrega = pedido.getDataHoraParaEntrega();
+        this.itensDePedido = pedido.getItensDePedido();
+        this.formaDeEntrega = pedido.getFormaDeEntrega();
+        this.formaDePagamento = pedido.getFormaDePagamento();
+        this.valorTotal = pedido.getValorTotal();
+        this.endereco = converteTipoEnderecoParaEnderecoDeEntrega(pedido);
+    }
+
+    private InformacoesDoCliente converteTipoClienteParaInformacoesCliente (Pedido pedido){
+        return new InformacoesDoCliente(pedido);
+    }
+    private EnderecoDeEntrega converteTipoEnderecoParaEnderecoDeEntrega(Pedido pedido){
+        return new EnderecoDeEntrega(pedido);
+    }
+
 }
