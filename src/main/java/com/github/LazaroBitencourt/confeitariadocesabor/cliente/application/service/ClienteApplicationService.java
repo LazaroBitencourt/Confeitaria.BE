e0ade472acd7183ad68fabaf69956cf227df5907
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-17 21:24:55 -0300

    modifica cliente-application-service

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/cliente/application/service/ClienteApplicationService.java
@@ -1,10 +1,12 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.service;
 
-import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClientRequest;
+import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteRequest;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.ClienteIdResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.api.DetalhaClienteResponse;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.repository.ClienteRepository;
 import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Cliente;
+import com.github.LazaroBitencourt.confeitariadocesabor.cliente.application.repository.EnderecoRepository;
+import com.github.LazaroBitencourt.confeitariadocesabor.cliente.domain.Endereco;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,13 @@ import java.util.UUID;
 public class ClienteApplicationService implements ClienteService{
 
     private final ClienteRepository repository;
+    private final EnderecoRepository enderecoRepository;
 
     @Override
-    public ClienteIdResponse cadastraNovoCliente(ClientRequest clientRequest) {
+    public ClienteIdResponse cadastraNovoCliente(ClienteRequest clienteRequest) {
         log.info("[inicia] ClienteApplicationService - cadastraNovoCliente");
-        Cliente novoCliente = repository.salva(new Cliente(clientRequest));
+        Endereco endereco = enderecoRepository.salva(new Endereco(clienteRequest));
+        Cliente novoCliente = repository.salva(new Cliente(clienteRequest, endereco));
         log.info("[finaliza] ClienteApplicationService - cadastraNovoCliente");
         return ClienteIdResponse.builder().idCliente(novoCliente.getIdCliente()).build();
     }
