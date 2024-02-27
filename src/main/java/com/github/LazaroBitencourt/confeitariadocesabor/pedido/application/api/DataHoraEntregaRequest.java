da0bd9d2829fcd14c5b5b4d650931cc6f3fbee67
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-27 10:52:35 -0300

    modifica data-hora-entrega-request

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/DataHoraEntregaRequest.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/application/api/DataHoraEntregaRequest.java
@@ -1,14 +1,12 @@
 package com.github.LazaroBitencourt.confeitariadocesabor.pedido.application.api;
 
 import lombok.Getter;
-import lombok.Value;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
-@Value
 public class DataHoraEntregaRequest {
     @NotNull
-    private LocalDateTime datahoraEntrega;
+    private LocalDateTime dataHoraParaEntrega;
 }
