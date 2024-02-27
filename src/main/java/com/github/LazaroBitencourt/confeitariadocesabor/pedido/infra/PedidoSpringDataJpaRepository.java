fc96f5e7a330954c4b310d9082183ef1ce06b168
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2024-02-26 21:02:22 -0300

    implementa metodo find-all-orders-by-id-client em pedido-spring-data-jpa-repository da camada de infra, testa fluxo, ok

--- src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/infra/PedidoSpringDataJpaRepository.java
+++ src/main/java/com/github/LazaroBitencourt/confeitariadocesabor/pedido/infra/PedidoSpringDataJpaRepository.java
@@ -3,12 +3,13 @@ package com.github.LazaroBitencourt.confeitariadocesabor.pedido.infra;
 import com.github.LazaroBitencourt.confeitariadocesabor.pedido.domain.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PedidoSpringDataJpaRepository extends JpaRepository<Pedido, UUID> {
 
-    @Query("SELECT p FROM Pedido p WHERE p.cliente.id = :idCliente")
-    List<Pedido> findAllOrderByIdClient(UUID idCliente);
+    @Query("SELECT p FROM Pedido p WHERE p.cliente.idCliente = :idCliente")
+    List<Pedido> findAllOrderByIdClient(@Param("idCliente") UUID idCliente);
 }
